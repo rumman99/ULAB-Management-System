c344176db7cae02de27a6d2427ef90b00dca43a1
rumman99 <tasnimrumman369@gmail.com>
2024-04-22 06:16:21 +0600

    student management system

---
+++ Transparent Form/DBconnect.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+
+namespace Transparent_Form
+{
+
+    class DBconnect
+    {
+        //to create connection
+        MySqlConnection connect = new MySqlConnection("datasource=localhost;port=3306;username=root;password='';database=studentdb");
+
+        //to get connection
+        public MySqlConnection getconnection
+        {
+            get
+            {
+                return connect;
+            }
+        }
+
+        //create a function to Open conncetion
+        public void openConnect()
+        {
+            if (connect.State == System.Data.ConnectionState.Closed)
+                connect.Open();
+        }
+
+        //Create a fuction to close connection
+        public void closeConnect()
+        {
+            if (connect.State == System.Data.ConnectionState.Open)
+                connect.Close();
+        }
+    }
+}
